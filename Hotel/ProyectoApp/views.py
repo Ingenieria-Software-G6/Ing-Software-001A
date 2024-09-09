3d2d61a85fd20fa8896a310c5baede1ac799d71d
segajardor <se.gajardor@duocuc.cl>
2024-09-09 11:05:50 -0300

    Cambios

--- Hotel/ProyectoApp/views.py
+++ Hotel/ProyectoApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Login exitoso")
+        else:
+            return HttpResponse("Credenciales incorrectas")
+    return render(request, 'login.html')
